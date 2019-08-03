94f3165a68f51fa08be6ef00d85b240b96a501af
scottishlacan <tyringebarn@yahoo.se>
2019-08-04 01:41:58 +0200

    Refactor array
    The array is never meant to be deleted. Instead the spots should be
    marked as busy or available, making it into a kind of memory pool.

--- src/utils/Array.cpp
+++ src/utils/Array.cpp
@@ -2,28 +2,69 @@
 
 #include <string.h>
 #include <stdlib.h>
-#include <assert.h>
+#include <intrin.h>
 
-Array NewArray(uint32_t sizeof_element, uint32_t num_elements) {
-  return Array{(uint8_t*)calloc(num_elements, sizeof_element), sizeof_element, 0, num_elements};
+int32_t* NewBusySpotRows(uint32_t numElements) {
+  int numFullRows = numElements / 32;
+  // In case we need more or less bits than what is divisible by the bit size
+  int partRow = (numElements % 32 > 0);
+  return (int32_t*)calloc(numFullRows + partRow, sizeof(int32_t));
 }
 
-void Push(Array* array, uint8_t element[], uint32_t sizeof_element) {
-  assert(array->sizeof_element == sizeof_element);
-
-  memcpy(&array->elements[array->size], element, sizeof_element);
-
-  array->size++;
+Array NewArray(uint32_t sizeOfElement, uint32_t numElements) {
+  return Array{
+    (uint8_t*)calloc(numElements, sizeOfElement),
+    sizeOfElement,
+    numElements,
+    NewBusySpotRows(numElements),
+  };
 }
 
-void Pop(Array* array)
+uint32_t Add(Array* array, uint8_t element[]) {
+  // Find the first busy spot row not holding only busy spots
+  uint32_t busySpotRow = 0;
+
+  // If busySpotRow hasn't incremented it means it has found an
+  // indicator to use
+  for (int i = 0; i == busySpotRow; i++) {
+    // If flipping all the bits is 0 it means all spots are marked as busy
+    busySpotRow += ~array->busy_spot_rows[i] == 0;
+  }
+
+  // Get the rightmost unset bit to find the first available not busy spot
+  int32_t freeBit = (array->busy_spot_rows[busySpotRow] + 1) & ~array->busy_spot_rows[busySpotRow];
+  unsigned long index;
+  // 2019-08-04: Currently available for these platforms - x86, ARM, x64
+  // If _BitScanReverse becomes outdated we could always use log2
+  _BitScanReverse(&index, freeBit);
+
+  memcpy(&array->elements[index * array->sizeof_element], element, array->sizeof_element);
+
+  // Mark the bit representing the element as busy
+  array->busy_spot_rows[busySpotRow] |= freeBit;
+
+  return index;
+}
+
+void RemoveElement(Array* array, uint32_t index)
 {
-  assert(array->size > 0);
-	array->size--;
+  // Mark the bit representing the element as not busy
+  array->busy_spot_rows[index / 32] &= ~(1 << (index % 32));
 }
 
-void ResizeArray(Array* array, uint32_t num_elements) {
-  array->elements = (uint8_t*)realloc(array->elements, array->sizeof_element * num_elements);
+void ResizeArray(Array* array, uint32_t numElements) {
+  array->elements = (uint8_t*)realloc(array->elements, array->sizeof_element * numElements);
 
-  array->max = num_elements;
+  int numFullRows = numElements / 32;
+  // In case we need more or less bits than what is divisible by the bit size
+  int partRow = (numElements % 32) > 0;
+  int numRows = numFullRows + partRow;
+  array->busy_spot_rows = (int32_t*)realloc(array->busy_spot_rows, sizeof(int32_t) * numRows);
+
+  array->max = numElements;
+}
+
+void FreeArray(Array* array) {
+  free(array->elements);
+  free(array->busy_spot_rows);
 }
--- src/utils/Array.h
+++ src/utils/Array.h
@@ -5,11 +5,18 @@
 struct Array {
   uint8_t* elements;
   uint32_t sizeof_element;
-  uint32_t size;
-  uint32_t  max;
+  uint32_t max;
+  int32_t* busy_spot_rows;
 };
 
-Array NewArray(uint32_t sizeof_element, uint32_t num_elements);
-void Push(Array* array, uint8_t element[], uint32_t sizeof_element);
-void Pop(Array* array);
-void ResizeArray(Array* array, uint32_t num_elements);
+Array NewArray(uint32_t sizeOfElement, uint32_t numElements);
+
+/// Returns the index of the element location in the array
+/// Marks the element index as busy
+uint32_t Add(Array* array, uint8_t element[]);
+
+/// Marks the element at index as not busy, meaning it is
+/// allowed to be overwritten
+void Remove(Array* array, uint32_t index);
+void ResizeArray(Array* array, uint32_t numElements);
+void FreeArray(Array* array);
