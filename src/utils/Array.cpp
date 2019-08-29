c139014a4aa082ef1eececb3f45d0319487496d5
scottishlacan <tyringebarn@yahoo.se>
2019-08-29 11:50:32 +0200

    Fixing array code a bit
    Creating some new public functions which may come to good use elsewhere.

--- src/utils/Array.cpp
+++ src/utils/Array.cpp
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <intrin.h>
 
-int32_t* NewBusySpotRows(uint32_t numElements) {
+int32_t* NewBusyMarkers(uint32_t numElements) {
   int numFullRows = numElements / 32;
   // In case we need more or less bits than what is divisible by the bit size
   int partRow = (numElements % 32 > 0);
@@ -16,11 +16,11 @@ Array NewArray(uint32_t sizeOfElement, uint32_t numElements) {
     (uint8_t*)calloc(numElements, sizeOfElement),
     sizeOfElement,
     numElements,
-    NewBusySpotRows(numElements),
+    NewBusyMarkers(numElements),
   };
 }
 
-uint32_t Add(Array array, uint8_t element[]) {
+unsigned long GetNewIndex(int32_t* busy_markers) {
   // Find the first busy spot row not holding only busy spots
   uint32_t busySpotRow = 0;
 
@@ -28,38 +28,45 @@ uint32_t Add(Array array, uint8_t element[]) {
   // indicator to use
   for (int i = 0; i == busySpotRow; i++) {
     // If flipping all the bits is 0 it means all spots are marked as busy
-    busySpotRow += ~array.busy_spot_rows[i] == 0;
+    busySpotRow += ~busy_markers[i] == 0;
   }
 
   // Get the rightmost unset bit to find the first available not busy spot
-  int32_t freeBit = (array.busy_spot_rows[busySpotRow] + 1) & ~array.busy_spot_rows[busySpotRow];
+  int32_t freeBit = (busy_markers[busySpotRow] + 1) & ~busy_markers[busySpotRow];
   unsigned long index;
   // 2019-08-04: Currently available for these platforms - x86, ARM, x64
   // If _BitScanReverse becomes outdated we could always use log2
   _BitScanReverse(&index, freeBit);
 
-  memcpy(&array.elements[index * array.sizeof_element], element, array.sizeof_element);
-
   // Mark the bit representing the element as busy
-  array.busy_spot_rows[busySpotRow] |= freeBit;
+  busy_markers[busySpotRow] |= freeBit;
 
   return index;
+}
+
+uint32_t Add(Array array, uint8_t element[]) {
+  int newElementIndex = GetNewIndex(array.busy_markers);
+
+  memcpy(&array.elements[newElementIndex * array.sizeof_element], element, array.sizeof_element);
+
+
+  return newElementIndex;
 }
 
 void RemoveElement(Array array, uint32_t index)
 {
   // Mark the bit representing the element as not busy
-  array.busy_spot_rows[index / 32] &= ~(1 << (index % 32));
+  array.busy_markers[index / sizeof(array.busy_markers[0])] &= ~(1 << (index % sizeof(array.busy_markers[0])));
 }
 
 Array ResizeArray(Array array, uint32_t numElements) {
   array.elements = (uint8_t*)realloc(array.elements, array.sizeof_element * numElements);
 
-  int numFullRows = numElements / 32;
+  int numFullRows = numElements / sizeof(array.busy_markers[0]);
   // In case we need more or less bits than what is divisible by the bit size
-  int partRow = (numElements % 32) > 0;
+  int partRow = (numElements % sizeof(array.busy_markers[0])) > 0;
   int numRows = numFullRows + partRow;
-  array.busy_spot_rows = (int32_t*)realloc(array.busy_spot_rows, sizeof(int32_t) * numRows);
+  array.busy_markers = (int32_t*)realloc(array.busy_markers, sizeof(int32_t) * numRows);
 
   array.max = numElements;
 
@@ -68,5 +75,5 @@ Array ResizeArray(Array array, uint32_t numElements) {
 
 void FreeArray(Array array) {
   free(array.elements);
-  free(array.busy_spot_rows);
+  free(array.busy_markers);
 }
--- src/utils/Array.h
+++ src/utils/Array.h
@@ -6,7 +6,7 @@ struct Array {
   uint8_t* elements;
   uint32_t sizeof_element;
   uint32_t max;
-  int32_t* busy_spot_rows;
+  int32_t* busy_markers;
 };
 
 Array NewArray(uint32_t sizeOfElement, uint32_t numElements);
@@ -22,3 +22,12 @@ void Remove(Array array, uint32_t index);
 /// Returns a resized array
 Array ResizeArray(Array array, uint32_t numElements);
 void FreeArray(Array array);
+
+/// Returns a new array of bits signifying busy or not busy
+/// array indicies by being 1 or 0.
+int32_t* NewBuysMarkers(uint32_t numElements);
+
+/// Finds the first not busy index in the array by finding the first
+/// 0 bit. Its position signifies the first not busy position in the
+/// array it's corresponding against.
+unsigned long GetNewIndex(int32_t* busy_markers);
