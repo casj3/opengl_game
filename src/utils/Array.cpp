dda73cde3736dd6e562a882123d1fb673a85ed31
scottishlacan <tyringebarn@yahoo.se>
2019-09-14 15:59:19 +0200

    Remove generic array struct
    The generic array struct is crap. Let's use casting instead, to and fro.

--- src/utils/Array.cpp
+++ src/utils/Array.cpp
@@ -11,16 +11,11 @@ int32_t* NewBusyMarkers(uint32_t numElements) {
   return (int32_t*)calloc(numFullRows + partRow, sizeof(int32_t));
 }
 
-Array NewArray(uint32_t sizeOfElement, uint32_t numElements) {
-  return Array{
-    (uint8_t*)calloc(numElements, sizeOfElement),
-    sizeOfElement,
-    numElements,
-    NewBusyMarkers(numElements),
-  };
+uint8_t* NewArray(uint32_t sizeOfElement, uint32_t numElements) {
+  return (uint8_t*)calloc(numElements, sizeOfElement);
 }
 
-unsigned long GetNewIndex(int32_t* busy_markers) {
+unsigned long GetFreeIndex(int32_t* busyMarkers) {
   // Find the first busy spot row not holding only busy spots
   uint32_t busySpotRow = 0;
 
@@ -28,52 +23,33 @@ unsigned long GetNewIndex(int32_t* busy_markers) {
   // indicator to use
   for (int i = 0; i == busySpotRow; i++) {
     // If flipping all the bits is 0 it means all spots are marked as busy
-    busySpotRow += ~busy_markers[i] == 0;
+    busySpotRow += ~busyMarkers[i] == 0;
   }
 
   // Get the rightmost unset bit to find the first available not busy spot
-  int32_t freeBit = (busy_markers[busySpotRow] + 1) & ~busy_markers[busySpotRow];
+  int32_t freeBit = (busyMarkers[busySpotRow] + 1) & ~busyMarkers[busySpotRow];
   unsigned long index;
   // 2019-08-04: Currently available for these platforms - x86, ARM, x64
   // If _BitScanReverse becomes outdated we could always use log2
   _BitScanReverse(&index, freeBit);
 
   // Mark the bit representing the element as busy
-  busy_markers[busySpotRow] |= freeBit;
+  busyMarkers[busySpotRow] |= freeBit;
 
   return index;
 }
 
-uint32_t Add(Array array, uint8_t element[]) {
-  int newElementIndex = GetNewIndex(array.busy_markers);
-
-  memcpy(&array.elements[newElementIndex * array.sizeof_element], element, array.sizeof_element);
-
-
-  return newElementIndex;
+void Add(uint8_t* array, uint8_t element[], uint32_t sizeOfElement, uint32_t freeIndex) {
+  memcpy(&array[freeIndex * sizeOfElement], element, sizeOfElement);
 }
 
-void RemoveElement(Array array, uint32_t index)
-{
-  // Mark the bit representing the element as not busy
-  array.busy_markers[index / sizeof(array.busy_markers[0])] &= ~(1 << (index % sizeof(array.busy_markers[0])));
+void Remove(uint8_t* array, uint32_t sizeOfElement, uint32_t removeIndex, uint32_t backIndex) {
+  // Overwrite it with the last element, we don't want to iterate through empty slots.
+  memcpy(&array[removeIndex * sizeOfElement],
+         &array[backIndex * sizeOfElement],
+         sizeOfElement);
 }
 
-Array ResizeArray(Array array, uint32_t numElements) {
-  array.elements = (uint8_t*)realloc(array.elements, array.sizeof_element * numElements);
-
-  int numFullRows = numElements / sizeof(array.busy_markers[0]);
-  // In case we need more or less bits than what is divisible by the bit size
-  int partRow = (numElements % sizeof(array.busy_markers[0])) > 0;
-  int numRows = numFullRows + partRow;
-  array.busy_markers = (int32_t*)realloc(array.busy_markers, sizeof(int32_t) * numRows);
-
-  array.max = numElements;
-
-  return array;
-}
-
-void FreeArray(Array array) {
-  free(array.elements);
-  free(array.busy_markers);
+uint8_t* ResizeArray(uint8_t* array, uint32_t sizeOfElement, uint32_t numElements) {
+  return (uint8_t*)realloc(array, sizeOfElement * numElements);
 }
--- src/utils/Array.h
+++ src/utils/Array.h
@@ -2,32 +2,47 @@
 
 #include <stdint.h>
 
-struct Array {
-  uint8_t* elements;
-  uint32_t sizeof_element;
-  uint32_t max;
-  int32_t* busy_markers;
-};
+/// Creates a new array. Meant to be cast to the chosen type.
+///
+/// @param sizeOfElement The size in bytes of the element type.
+/// @param numelElements The number of elements to allocate space for.
+uint8_t* NewArray(uint32_t sizeOfElement, uint32_t numElements);
 
-Array NewArray(uint32_t sizeOfElement, uint32_t numElements);
+/// Adds an element to the array at the specified location.
+///
+/// @param array Pointer to array.
+/// @param element The element to add.
+/// @param sizeOfElement The size in bytes of the element type.
+/// @param freeIndex The index in the array where the element will be added.
+void Add(uint8_t* array, uint8_t element[], uint32_t sizeOfElement, uint32_t freeIndex);
 
-/// Returns the index of the element location in the array
-/// Marks the element index as busy
-uint32_t Add(Array array, uint8_t element[]);
+/// Removes the element at the given index by replacing it with the last written element
+/// in the array. The last element is therefore moved to the place of the removed element.
+///
+/// @param array Pointer to array.
+/// @param sizeOfElement The size in bytes of the element type.
+/// @param removeIndex The index of the element which will be replaced by the last written element.
+/// @param backIndex The index of the element in the back of the array which will replace the
+///                  removed element.
+void Remove(uint8_t* array, uint32_t sizeOfElement, uint32_t removeIndex, uint32_t backIndex);
 
-/// Marks the element at index as not busy, meaning it is
-/// allowed to be overwritten
-void Remove(Array array, uint32_t index);
-
-/// Returns a resized array
-Array ResizeArray(Array array, uint32_t numElements);
-void FreeArray(Array array);
+/// Returns a resized array. Meant to be cast to the chosen type.
+///
+/// @param array Pointer to array.
+/// @param sizeOfElement The size in bytes of the element type.
+/// @param numelElements The number of elements to allocate space for.
+uint8_t* ResizeArray(uint8_t* array, uint32_t sizeOfElement, uint32_t numElements);
 
 /// Returns a new array of bits signifying busy or not busy
 /// array indicies by being 1 or 0.
+///
+/// @param numElements The number of elements to create a collection of
+///                    busy markers for.
 int32_t* NewBuysMarkers(uint32_t numElements);
 
 /// Finds the first not busy index in the array by finding the first
 /// 0 bit. Its position signifies the first not busy position in the
 /// array it's corresponding against.
-unsigned long GetNewIndex(int32_t* busy_markers);
+///
+/// @return busyMarkers The collection of busy bits.
+unsigned long GetFreeIndex(int32_t* busyMarkers);
